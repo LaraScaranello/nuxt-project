9c7a3e24a0cbbd62a7d7353a68f352c385400d6a
Lara Scaranello <ti@grindelia.com.br>
2025-04-22 10:19:20 -0300

    routes

--- app.vue
+++ app.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    Hello Nuxt 3
+    <NuxtLayout>
+      <header>
+        <ul>
+          <li>
+            <NuxtLink to="/">Home</NuxtLink>
+            <NuxtLink to="/events">Events</NuxtLink>
+          </li>
+        </ul>
+      </header>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,6 @@
 import { resolve } from 'path';
 import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
-  alias: {
-    '@': resolve(__dirname, '/'),
-  },
   css: ["~/assets/main.css"],
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
